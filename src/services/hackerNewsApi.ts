a3527c1f37504023d40aa4ce55262256ab3325f9
dlwegscheid <dlwegscheid@gmail.com>
2020-07-02 13:48:44 -0400

    pull out story type

--- src/services/hackerNewsApi.ts
+++ src/services/hackerNewsApi.ts
@@ -1,10 +1,10 @@
-import {IStory} from '../contexts/StoriesProvider';
+import {Story} from '../types/story';
 
 const baseUrl = 'https://hacker-news.firebaseio.com/v0';
 
 export const getStoryDetails = async (storyId: number) => {
   const response = await fetch(`${baseUrl}/item/${storyId}.json`);
-  return await response.json() as IStory;
+  return await response.json() as Story;
 }
 
 export const getStoryList = async () => {
