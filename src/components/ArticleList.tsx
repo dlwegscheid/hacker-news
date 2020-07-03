84a6a9175a8b6feaf998cddabcc16472ce668c12
dlwegscheid <dlwegscheid@gmail.com>
2020-07-02 20:57:14 -0400

    use reactist for loading spinner

--- src/components/ArticleList.tsx
+++ src/components/ArticleList.tsx
@@ -1,9 +1,20 @@
 import React, {useContext} from 'react';
 import InfiniteScroll from 'react-infinite-scroller';
+import styled from 'styled-components';
+import {Loading} from '@doist/reactist'
 import {getStoryDetails} from '../services/hackerNewsApi';
 import {StoriesContext, Action} from '../contexts/StoriesProvider';
 import Story from './Story';
 import {StoryModel} from '../types/story';
+
+const ListWrapper = styled.main`
+  background-color: #F5F6F8;
+  padding 1rem 2rem;
+`;
+
+const LoadingWrapper = styled.div`
+  margin: 1rem;
+`;
 
 const loadFunc = (stories: Map<number, StoryModel | undefined>, dispatch: React.Dispatch<Action>) => async (index: number) => {
   const newStory = await getStoryDetails(Array.from(stories.keys())[index - 1]);
@@ -12,18 +23,19 @@ const loadFunc = (stories: Map<number, StoryModel | undefined>, dispatch: React.
 
 export default function ArticleList() {
   const {stories, dispatch} = useContext(StoriesContext);
-  console.log(stories);
   const details = Array.from(stories.values()).filter(v => v) as StoryModel[];
 
-  return <ol>
-    {stories.size > 0 && <InfiniteScroll
-      pageStart={0}
-      loadMore={loadFunc(stories, dispatch)}
-      hasMore={details.length < 500}
-      loader={<div className="loader" key={0}>Loading...</div>}
-    >
-      {details.map(s => <Story story={s}></Story>)}
-    </InfiniteScroll>
-    }
-  </ol>
+  return <ListWrapper>
+    <ol>
+      {stories.size > 0 && <InfiniteScroll
+        pageStart={0}
+        loadMore={loadFunc(stories, dispatch)}
+        hasMore={details.length < 500}
+        loader={<div className="loader" key={0}><LoadingWrapper><Loading /></LoadingWrapper></div>}
+      >
+        {details.map(s => <Story key={s.id} story={s}></Story>)}
+      </InfiniteScroll>
+      }
+    </ol>
+  </ListWrapper>
 }
