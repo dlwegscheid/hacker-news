71c7e092597a8e930430cfba089270d0c705ec19
dlwegscheid <dlwegscheid@gmail.com>
2020-07-02 20:15:26 -0400

    + header

---
+++ src/components/Header.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const HeaderNav = styled.nav`
+  text-align: center;
+  background-color: #C0C7CB;
+  padding: 1rem 0;
+  border
+`;
+
+const HomeLink = styled.a`
+  text-decoration: none;
+  color: #2E3940;
+`;
+
+export default function Header() {
+  return <HeaderNav>
+    <h1>
+      <HomeLink href="/">
+        David's HackerNews
+      </HomeLink>
+    </h1>
+  </HeaderNav>;
+}
